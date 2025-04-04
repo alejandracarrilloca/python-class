5b3f8e30f9f2da93a677faeaa0b8c8a805345511
AlejandraCarrillo <carrillo@lcg.unam.mx>
2025-04-03 20:17:50 -0600

    Ejercicio 7

---
+++ bin /7_invertir.py
@@ -0,0 +1,3 @@
+secuencias = input("Ingresa una secuencias separadas por comas:").split(",")
+secuencias_inversas = [secuencia [::-1] for secuencia in secuencias]
+print(f"\nSecuencia inversa: {secuencias_inversas}")
